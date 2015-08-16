e08760613c37380dd19e498148d25c1fce4d5100
Craig Heydenburg <craigh@mac.com>
2015-08-16 17:40:44 -0400

    convert ThemeModule

---
+++ Resources/public/js/ZikulaThemeModule.Admin.View.js
@@ -0,0 +1,7 @@
+// Copyright Zikula Foundation 2014 - license GNU/LGPLv3 (or at your option, any later version).
+
+( function($) {
+    $(document).ready(function() {
+        $('a.marktooltip').popover();
+    });
+})(jQuery);
